339a773ea72eebb2d897bacbc29a031eefadf4a5
Maxime Dupuis <dupuismaxime@hotmail.com>
2022-01-09 22:03:51 -0500

    OnStateExit destroy warrior shadow

--- Assets/AnimationAutoDestroy.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-public class AnimationAutoDestroy : MonoBehaviour {
-    void Start () {
-        Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length - 0.1f); 
-    }
-}
