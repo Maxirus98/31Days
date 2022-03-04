d7572e1b0285d00bcc795f92e5652c8e4f5b95af
Maxime <dupuismaxime@hotmail.com>
2022-03-03 20:42:36 -0500

    Add Player Live Camera View Ui

---
+++ Assets/RenderFromCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderFromCamera : MonoBehaviour
+{
+    private Camera _camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GameObject.FindWithTag("PlayerUiCamera").GetComponent<Camera>();
+        _camera.forceIntoRenderTexture = true;
+    }
+}
---
+++ Assets/SelectPlayerUiRenderer.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SelectPlayerUiRenderer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]private TextMeshProUGUI _playerClassText;
+    void Start()
+    {
+        var player = GameObject.FindWithTag("Player");
+        var playerName = player.name;
+        var sanitizedName = player.name.Substring(0, playerName.IndexOf('('));
+        _playerClassText.text = sanitizedName;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            if (child.name.Contains(sanitizedName))
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
