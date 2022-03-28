e9ad41907f4c430262a50f42834bd906dafe2d1a
Maxime <dupuismaxime@hotmail.com>
2022-03-28 05:15:05 -0400

    Add tooltip, to finish

---
+++ Assets/Tooltip.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    #region string access region
+    private readonly string TOOLTIP_BACKGROUND = "Background";
+    private readonly string TOOLTIP_TEXT = "Text";
+    private readonly string PLAYER_CAMERA = "PlayerCamera";
+    #endregion
+    
+    private TextMeshProUGUI _tooltipText;
+    private RectTransform _backgroundRectTransform;
+    private Camera _playerCamera;
+    
+    protected void Awake()
+    {
+        _backgroundRectTransform = transform.Find(TOOLTIP_BACKGROUND).GetComponent<RectTransform>();
+        _tooltipText = transform.Find(TOOLTIP_TEXT).GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        _playerCamera = GameObject.Find(PLAYER_CAMERA).GetComponent<Camera>();
+        Hide();
+    }
+
+    private void Update()
+    {
+        MoveTooltipToMouse();
+    }
+
+    private void MoveTooltipToMouse()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), Input.mousePosition, _playerCamera, out Vector2 localPoint);
+        transform.localPosition = localPoint;
+    }
+
+    public void Show(string content)
+    {
+        _tooltipText.text = content;
+        var textPaddingSize = 2f;
+        Vector2 backgroundSize = new Vector2(_tooltipText.preferredWidth + textPaddingSize, _tooltipText.preferredHeight + textPaddingSize);
+        _backgroundRectTransform.sizeDelta = backgroundSize;
+        gameObject.SetActive(true);
+    }
+    
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
