36afa3d5d57343d82780da0649089969156921aa
Maxirus98 <dupuismaxime@hotmail.com>
2022-06-13 06:58:56 -0400

    Moved folders

--- Assets/Scripts/DestroyOnExit.cs
+++ Assets/Scripts/DestroyOnExit.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Is on ShadowCleaver Warrior Animation
+/// </summary>
 public class DestroyOnExit : StateMachineBehaviour {
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         Destroy(animator.gameObject, stateInfo.length);
